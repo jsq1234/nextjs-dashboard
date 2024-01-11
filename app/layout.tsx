354ec8ff32426f3e6108db22729737442079631b
jsq1234 <mananprakash3078@gmail.com>
2024-01-11 15:55:27 +0530

    added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The Next.js dashboard built using App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
 export default function RootLayout({
   children,
 }: {
